2302d23dd49487aef6790b262611ffee60c9238e
Laimonas Masionis <71811684+LaimonasMas@users.noreply.github.com>
2020-12-02 17:28:42 +0200

    Supildyta

--- js/main.js
+++ js/main.js
@@ -1,2 +1,10 @@
-import { sumavimas } from "./components/matematika/sumavimas.js";
+import * as Matke from './components/matematika/matematika.js';
+
+console.log(Matke);
+
+console.log(Matke.sumavimas(5, 48));
+console.log(Matke.atimtis(5, 48));
+console.log(Matke.daugyba(5, 48));
+console.log(Matke.dalyba(5, 48));
+console.log(Matke.kvadratu(5));
 
